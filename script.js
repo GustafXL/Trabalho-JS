cb562d7f4001d21602a01da2b3db19e57589010b
Gustavo Linhares <gustavolinharescr7@gmail.com>
2022-08-02 14:30:37 -0300

    Código alterado e funcionando!

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function carregar() {
-    var a = document.getElementById('ano')
-    var gen = document.getElementById('genero')
-    var year = new Date()
-    var data = year.getFullYear
-    
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('ano')
+    if (fano.value.length == 0 || fano.value > ano) {
+        alert('[ERRO] Verifique se os Dados estão corretos!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `Idade calculada: ${idade}`
+    }
 }
