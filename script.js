863b90ab21305c617b9329bdf5eb34763e2b4a54
Gustavo Linhares <gustavolinharescr7@gmail.com>
2022-08-02 11:52:33 -0300

    imagens cortadas e alteradas

---
+++ script.js
@@ -0,0 +1,7 @@
+function carregar() {
+    var a = document.getElementById('ano')
+    var gen = document.getElementById('genero')
+    var year = new Date()
+    var data = year.getFullYear
+    
+}
